13fe102e06eca28bbaf8829a80be5b40a1fbd07f
NadiatuWafrina <nadiawafrina99@gmail.com>
2021-01-14 18:41:38 +0700

    .

---
+++ Menu.php
@@ -0,0 +1,28 @@
+<?php
+class Menu{
+    public $rawon  ;
+    public $soto ;
+   public $nasicampur;
+
+function getRawon(){
+   return $this->rawon;
+}
+function setRawon($rawon){
+   $this->rawon = 13.000;
+}
+function getSoto(){
+   return $this->soto;
+}
+function setSoto($soto){
+   $this->soto = 20.000;
+}
+function getNasicampur($nasicampur){
+  return $this->nasicampur;
+}
+function setNasicampur($nasicampur){
+   $this->nasicampur= 20.000;
+}
+}
+?>
+
+
